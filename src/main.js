349ca9c36b917867c946bfefbd634727c03b3253
Vasilkrug <vkruglovec@gmail.com>
2022-04-02 13:31:02 +0300

    add button, addEventListener

--- src/main.js
+++ src/main.js
@@ -2,3 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+const button = document.querySelector('.btn-acc')
+
+button.addEventListener('click', ()=> {
+    console.log('это личный кабинет пользователя')
+})
